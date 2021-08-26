9c1663dfbf0e5ee52825ff6cb28aff160e1f6671
agentSnowball <detectiveboudreaux@gmail.com>
2021-08-26 00:23:15 +0000

    Wires now hates genshin impact, he will hiss at any mention of it. also he likes food!

--- main.py
+++ main.py
@@ -15,8 +15,21 @@ async def on_message(message):
   if (message.author == client.user):
     return
 
-  if (message.content.lower().startswith("wires")):
+  if "wires" in message.content.lower():
     time.sleep(2.5)
-    await message.channel.send('meow.')
+    await message.channel.send('meow')
+
+  if "genshin" in message.content.lower() or "impact" in message.content.lower():
+    time.sleep(1.5)
+    await message.channel.send("*hissssssssssssss*")
+
+  if "food" in message.content.lower() and "genshin" not in message.content.lower() and "impact" not in message.content.lower():
+    time.sleep(2.5)
+    await message.channel.send("(*runs towards you for snack!*)")
+
+  if "meow" in message.content.lower():
+    time.sleep(2.5)
+    await message.channel.send("meow")
+
 
 client.run(os.getenv('TOKEN'))
